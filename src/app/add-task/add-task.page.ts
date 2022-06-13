1eae9ec78d777c04a2e4088618b3547460bf89a3
Sanjai Sakthivel <sanjaisakthivel2000@gmail.com>
2022-06-13 15:27:40 +0530

    feat(ios): added ios files

--- src/app/add-task/add-task.page.ts
+++ src/app/add-task/add-task.page.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl,FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.page.html',
   styleUrls: ['./add-task.page.scss'],
 })
 export class AddTaskPage implements OnInit {
+public addTaskForm = new FormGroup({
+date: new FormControl(''),
+label: new FormControl(''),
+taskType: new FormControl(''),
+workLog:new FormControl(''),
+taskName: new FormControl(''),
+taskNumber: new FormControl(''),
+taskDescription: new FormControl('')
+});
+postId;
 
-  constructor() { }
+constructor(private http: HttpClient,public toastController: ToastController) { }
 
   ngOnInit() {
   }
+  async presentToast() {
+    this.addTaskForm.reset();
+    const toast = await this.toastController.create({
+      message: 'Your task is saved successfully.',
+      duration: 2000,
+      color: 'success',
+    });
+    toast.present();
+  }
 
+public addTask(){
+
+if(this.addTaskForm.value.taskName === 'Learning'){
+  this.addTaskForm.value.taskNumber = undefined;
+}
+this.http.post<any>('https://work-master-pro.herokuapp.com/addtask', this.addTaskForm.value).subscribe(data => {
+      this.presentToast();
+    });
+}
 }
