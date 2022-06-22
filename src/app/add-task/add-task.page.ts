5c621d8363e224ee5dbcb45b9f4972f6b0a6d683
Sanjai Sakthivel <sanjaisakthivel2000@gamil.com>
2022-06-22 20:59:50 +0530

    feat(android): added android app

--- src/app/add-task/add-task.page.ts
+++ src/app/add-task/add-task.page.ts
@@ -1,28 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl,FormGroup } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { ToastController } from '@ionic/angular';
+import * as moment from 'moment';
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.page.html',
   styleUrls: ['./add-task.page.scss'],
 })
 export class AddTaskPage implements OnInit {
-public addTaskForm = new FormGroup({
-date: new FormControl(''),
-label: new FormControl(''),
-taskType: new FormControl(''),
-workLog:new FormControl(''),
-taskName: new FormControl(''),
-taskNumber: new FormControl(''),
-taskDescription: new FormControl('')
-});
-postId;
+  public addTaskForm = new FormGroup({
+    date: new FormControl(''),
+    label: new FormControl(''),
+    taskType: new FormControl(''),
+    workLog: new FormControl(''),
+    taskName: new FormControl(''),
+    taskNumber: new FormControl(''),
+    taskDescription: new FormControl(''),
+  });
+  postId;
 
-constructor(private http: HttpClient,public toastController: ToastController) { }
+  constructor(
+    private http: HttpClient,
+    public toastController: ToastController,
+    private router: Router
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
   async successToast() {
     this.addTaskForm.reset();
     const toast = await this.toastController.create({
@@ -43,21 +48,29 @@ constructor(private http: HttpClient,public toastController: ToastController) { 
     toast.present();
   }
 
-public addTask(){
+  public addTask() {
+    if (this.addTaskForm.value.taskName === 'Learning') {
+      this.addTaskForm.value.taskNumber = undefined;
+    }
+    this.addTaskForm.value.date = moment(this.addTaskForm.value.date).format(
+      'MM-DD-YYYY'
+    );
+    // this.http.post<any>('https://work-master-pro.herokuapp.com/addtask', this.addTaskForm.value).subscribe(data => {
+    //       this.presentToast();
+    //     });
 
-if(this.addTaskForm.value.taskName === 'Learning'){
-  this.addTaskForm.value.taskNumber = undefined;
-}
-// this.http.post<any>('https://work-master-pro.herokuapp.com/addtask', this.addTaskForm.value).subscribe(data => {
-//       this.presentToast();
-//     });
-
-    this.http.post<any>('https://work-master20.herokuapp.com/addtask', this.addTaskForm.value).subscribe(data => {
-      if(data){
-      this.successToast();}
-      else{
-        this.failedToast();
-      }
-    });
-}
+    this.http
+      .post<any>(
+        'https://work-master20.herokuapp.com/addtask',
+        this.addTaskForm.value
+      )
+      .subscribe((data) => {
+        if (data) {
+          this.successToast();
+          this.router.navigateByUrl('/view-work');
+        } else {
+          this.failedToast();
+        }
+      });
+  }
 }
--- src/app/view-work/view-work.page.ts
+++ src/app/view-work/view-work.page.ts
@@ -1,7 +1,12 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import {
+  Component,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { IonInfiniteScroll } from '@ionic/angular';
-import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-view-work',
   templateUrl: './view-work.page.html',
@@ -17,40 +22,48 @@ export class ViewWorkPage implements OnInit {
   taskInfo;
   taskData: any[] = [];
   description = 'added integration of m1 ';
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
   ngOnInit() {
     this.getData();
-
   }
   /*
   https://work-master-pro.herokuapp.com/*/
-  async getData(){
-    await this.http.get<any>('https://work-master20.herokuapp.com/getTask').subscribe(data => {
-      this.taskInfo = data;
-      this.taskInfo.sort((a,b)=> {
-        const d1 = Date.parse(a.date);
-        const d2 = Date.parse(b.date);
-        if(d1 <d2){
-          return -1;
-        }
-        if(d1> d2){
-          return 1;
-        }
-        return 0;
-      }
-      );
-      this.taskData =[];
-      const dataBasedOnDat = this.taskInfo.filter((eachtask) => eachtask.date === this.taskInfo[0].date);
-      this.taskData.push({date: this.taskInfo[0].date,value:dataBasedOnDat });
-      for(let i = 1 ; i < Object.keys(this.taskInfo).length ; i++){
-          if (this.taskInfo[i].date !== this.taskInfo[i-1].date ){
-            const dataBasedOnDate = this.taskInfo.filter((eachtask) => eachtask.date === this.taskInfo[i].date);
-            this.taskData.push({date: this.taskInfo[i].date,value:dataBasedOnDate });
+  async getData() {
+    await this.http
+      .get<any>('https://work-master20.herokuapp.com/getTask')
+      .subscribe((data) => {
+        this.taskInfo = data;
+        this.taskInfo.sort((a, b) => {
+          const d1 = Date.parse(a.date);
+          const d2 = Date.parse(b.date);
+          if (d1 < d2) {
+            return -1;
           }
-}
-console.log(this.taskData);
-    });
-
+          if (d1 > d2) {
+            return 1;
+          }
+          return 0;
+        });
+        this.taskData = [];
+        const dataBasedOnDat = this.taskInfo.filter(
+          (eachtask) => eachtask.date === this.taskInfo[0].date
+        );
+        this.taskData.push({
+          date: this.taskInfo[0].date,
+          value: dataBasedOnDat,
+        });
+        for (let i = 1; i < Object.keys(this.taskInfo).length; i++) {
+          if (this.taskInfo[i].date !== this.taskInfo[i - 1].date) {
+            const dataBasedOnDate = this.taskInfo.filter(
+              (eachtask) => eachtask.date === this.taskInfo[i].date
+            );
+            this.taskData.push({
+              date: this.taskInfo[i].date,
+              value: dataBasedOnDate,
+            });
+          }
+        }
+        console.log(this.taskData);
+      });
   }
 }
